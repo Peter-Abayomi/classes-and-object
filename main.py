63958e1107be0e35735d33c2cf07d1fdf5d4da67
Abayomi <89710410+Peter-Abayomi@users.noreply.github.com>
2022-06-03 06:40:12 +0100

    fixed all errors

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
 class Student:
     # [assignment] Skeleton class. Add your code here
-    def __init__(self):
-        pass
+    def __init__(self, name, age, tracks, score):
+        #Instance Variable
+        self.name = name
+        self.age = age
+        self.tracks = tracks
+        self.score = score
 
+    def change_name(self):
+        print("Peter")
 
+    def change_age(self):
+        print("34")
+
+    def add_track(self):
+        print("UI/UX")
+
+    def get_score(self):
+        print("20.90")
 
 Bob = Student(name="Bob", age=26, tracks=["FE","BE"],score=20.90)
 
 # Expected methods
-Bob.change_name("Peter")
-Bob.change_age(34)
-Bob.add_track("UI/UX")
+Bob.change_name()
+Bob.change_age()
+Bob.add_track()
 Bob.get_score()
